98163f6b1555fba9e277c617ba90af8f04626785
Keidi <kk2314@ic.ac.uk>
2019-01-21 19:13:41 +0000

    Label data

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import numpy as np
-import sys
-import time
-import os
-import re 
-f = open('data/0750am-0805am/trajectories-0750am-0805am.txt', 'rt')
-
-lines = f.read().split('    ')
 import pandas as pd
-data = pd.read_csv('data/0750am-0805am/trajectories-0750am-0805am.txt', delim_whitespace=True, header=None)
+pd.set_option('display.mpl_style', 'default')
+pd.set_option('display.width', 5000) 
+pd.set_option('display.max_columns', 60)
+
+data_first = pd.read_csv('data/0750am-0805am/trajectories-0750am-0805am.txt', delim_whitespace=True, header=None)
+data_first.columns = ['Vehicle ID','Frame ID','Total Frames','Global Time','Local X','Local Y','Global X', 'Global Y','Vehicle Length','Vehicle Width', 'Vehicle Class', 'Vehicle Velocity', 'Vehicle Acceleration', 'Lane Identification', 'Preceding Vehicle', 'Following Vehicle', 'Spacing', 'Headway ']
+data_first['Vehicle ID'].unique().size
