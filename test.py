bda86779342afccea28566edb974a1400c16e0e5
Kapllani <kk2314@ic.ac.uk>
2019-01-21 13:16:00 +0000

    Import data

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-https://we.tl/t-1TnGaJd1Js
+import numpy as np
+import sys
+import time
+import os
+import re 
+f = open('data/0750am-0805am/trajectories-0750am-0805am.txt', 'rt')
+
+lines = f.read().split('    ')
+import pandas as pd
+data = pd.read_csv('data/0750am-0805am/trajectories-0750am-0805am.txt', delim_whitespace=True, header=None)
